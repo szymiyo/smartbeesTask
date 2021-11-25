79bbdc67eb3f533efb4e74fe5430a94ca4b30b57
szymonn7 <szymon@swiderscy.com>
2021-11-25 20:08:25 +0100

    first look of models

--- database/migrations/2021_11_25_174214_create_orders_table.php
+++ database/migrations/2021_11_25_174214_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('login');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->int('user_id');
+            $table->decimal('amount', 10, 2);
             $table->int('address_id');
-            $table->rememberToken();
+            $table->int('payment_method_id');
+            $table->int('delivery_method_id');
             $table->timestamps();
         });
     }
@@ -34,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 }
